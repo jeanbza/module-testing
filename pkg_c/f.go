ffffeae52118c0fb64bb4b2c3d3574a3c7fd02e0
Jean de Klerk <deklerk@google.com>
2018-11-14 15:59:20 -0800

    pkg_c depends on parent: pkg_c v2.0.1

--- pkg_c/f.go
+++ pkg_c/f.go
@@ -1,4 +1,8 @@
 package pkg_c
 
+import "github.com/jadekler/module-testing"
+
 const FooC = "FooC"
 const FooCC = "FooCC"
+
+var _ = modules_multi.FooMain
--- pkg_c/go.mod
+++ pkg_c/go.mod
@@ -1,1 +1,3 @@
 module github.com/jadekler/module-testing/pkg_c/v2
+
+require github.com/jadekler/module-testing v0.0.2
