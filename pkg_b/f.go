d785a765ebe1d9c2c0f14b08776c0873d3db7190
Jean de Klerk <deklerk@google.com>
2018-11-15 08:06:22 -0800

    pkg_b: depend on main@v0.0.2, tag pkg_b/v0.0.4

--- pkg_b/f.go
+++ pkg_b/f.go
@@ -1,5 +1,9 @@
 package pkg_b
 
+import "github.com/jadekler/module-testing"
+
+var _ = modules_multi.FooMain
+
 const FooB = "FooB version v0.0.3"
 
 const FooB2 = "FooB2 version v0.0.3"
--- pkg_b/go.mod
+++ pkg_b/go.mod
@@ -1,1 +1,3 @@
 module github.com/jadekler/module-testing/pkg_b
+
+require github.com/jadekler/module-testing v0.0.2
