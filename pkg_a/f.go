6b188db1ca7f80faa5d9994828f7b3c98d82a7a4
Jean de Klerk <deklerk@google.com>
2018-11-19 16:27:19 -0800

    add internal, pkg_a depends on internal, pkg_a/v0.0.2, main v0.0.3

--- pkg_a/f.go
+++ pkg_a/f.go
@@ -1,7 +1,11 @@
 package pkg_a
 
-import "github.com/jadekler/module-testing/pkg_b"
+import (
+	"github.com/jadekler/module-testing/pkg_b"
+	"github.com/jadekler/module-testing/internal"
+)
 
 const FooA = "FooA"
 
 var _ = pkg_b.FooB
+var _= internal.InternalThing
